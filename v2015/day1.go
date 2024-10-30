a7ab2d740d6707ff92ff7c46c75c5a3b56ca086f
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2024-10-31 00:23:23 +0200

    init

--- v2015/day1.go
+++ v2015/day1.go
@@ -13,6 +13,13 @@ func Run() {
 		log.Fatal(err)
 	}
 
+	fmt.Println("2015: Day 1, Part 1: Not Quite Lisp")
+	fmt.Println(floor(input))
+	fmt.Println("2015: Day 1, Part 2: Not Quite Lisp")
+	fmt.Println(basement(input))
+}
+
+func floor(input string) int {
 	var counter int
 
 	for _, r := range input {
@@ -23,5 +30,23 @@ func Run() {
 		}
 	}
 
-	fmt.Println("Part 1: ", counter)
+	return counter
+}
+
+func basement(input string) int {
+	var counter int
+
+	for i, r := range input {
+		if r == '(' {
+			counter++
+		} else {
+			counter--
+		}
+
+		if counter == -1 {
+			return i + 1
+		}
+	}
+
+	return -1
 }
